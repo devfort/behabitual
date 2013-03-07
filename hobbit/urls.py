b1655c2313c0e8a00827a42e9db77515a19f456d
mazz <marianne.mosley@gmail.com>
2013-03-07 17:19:50 +0000

    Change yo password

--- hobbit/urls.py
+++ hobbit/urls.py
@@ -11,6 +11,8 @@ urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='homepage/home.html'), name='homepage'),
     url(r'^login/$', login, {'template_name': 'accounts/login.html'}, name='login'),
     url(r'^logout/$', accounts.views.LogoutView.as_view(), name='logout'),
+    url(r'^password-change/$', 'django.contrib.auth.views.password_change', name='password_change'),
+    url(r'^password-change/done/$', 'django.contrib.auth.views.password_change_done', name='password_change_done'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
