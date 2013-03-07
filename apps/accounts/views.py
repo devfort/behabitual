e1ee6a1bc191bd955241e2edd451e8c7db90deb7
mazz <marianne.mosley@gmail.com>
2013-03-07 16:27:21 +0000

    Allow logging out

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,1 +1,22 @@
+from django.contrib.auth import logout
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.views.generic import TemplateView
 
+
+class LogoutView(TemplateView):
+    """
+    Logs the user out.
+    """
+
+    template_name = "accounts/logout.html"
+
+    def get_next_url(self):
+        next = self.request.POST.get("next", None)
+        if next is None:
+            next = reverse('homepage')
+        return next
+
+    def post(self, request):
+        logout(self.request)
+        return HttpResponseRedirect(self.get_next_url())
--- hobbit/urls.py
+++ hobbit/urls.py
@@ -1,11 +1,16 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.auth.views import login
+from django.views.generic import TemplateView
+
+import accounts.views
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^login/$', login, {'template_name': 'accounts/login.html'}),
+    url(r'^$', TemplateView.as_view(template_name='homepage/home.html'), name='homepage'),
+    url(r'^login/$', login, {'template_name': 'accounts/login.html'}, name='login'),
+    url(r'^logout/$', accounts.views.LogoutView.as_view(), name='logout'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
