f1097520bd93d54837afab99b569f8c958c7df56
mazz <marianne.mosley@gmail.com>
2013-03-07 15:44:53 +0000

    First pass at Login. BOOM

--- hobbit/urls.py
+++ hobbit/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.auth.views import login
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^login/$', login, {'template_name': 'accounts/login.html'}),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
