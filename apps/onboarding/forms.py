96369066b592094df09127fdf9f5a980e96209ae
James Coglan <jcoglan@gmail.com>
2013-03-09 16:21:18 +0000

    Second form in the onboarding flow, to tell us when the user wants us to send them email.

--- apps/onboarding/forms.py
+++ apps/onboarding/forms.py
@@ -1,6 +1,20 @@
 from django import forms
 from apps.habits.models import RESOLUTION_CHOICES
 
+DAYS_OF_WEEK = (
+    ('MONDAY', 'Monday'),
+    ('TUESDAY', 'Tuesday'),
+    ('WEDNESDAY', 'Wednesday'),
+    ('THURSDAY', 'Thursday'),
+    ('FRIDAY', 'Friday'),
+    ('SATURDAY', 'Saturday'),
+    ('SUNDAY', 'Sunday'),
+)
+
+HOURS = zip(range(24), range(24))
+MINUTES = zip(range(60), range(60))
+
+
 class HabitForm(forms.Form):
     description = forms.CharField(max_length=100)
     target_value = forms.IntegerField()
@@ -10,7 +24,14 @@ class HabitForm(forms.Form):
 
 
 class ReminderForm(forms.Form):
-    pass
+    email = forms.EmailField()
+    trigger = forms.CharField(max_length=50)
+    days = forms.MultipleChoiceField(
+        choices=DAYS_OF_WEEK,
+        widget=forms.CheckboxSelectMultiple,
+    )
+    hour = forms.ChoiceField(choices=HOURS)
+    minute = forms.ChoiceField(choices=MINUTES)
 
 
 class SummaryForm(forms.Form):
