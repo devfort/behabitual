f3d03e007314538005f299c0678b8ef4500682cf
Nick Stenning <nick@whiteink.com>
2013-03-09 13:45:38 +0100

    Add a bit more explanation of attach_fixture_tests

--- lib/test_helpers.py
+++ lib/test_helpers.py
@@ -5,6 +5,16 @@ def attach_fixture_tests(test_cls, test_func, fixtures):
     For each fixture in the iterable ``fixtures``, attach the test function
     ``test_func`` using that fixture data to the specified test class
     (``test_cls``).
+
+    Expects test_func to have the signature of an instance method taking one
+    parameter for the fixture data, e.g.::
+
+        FIXTURES = [1,2,3]
+
+        def test_foo(self, fix):
+            self.assertTrue(fix in range(10))
+
+        attach_fixture_tests(TestFoo, test_foo, FIXTURES)
     """
     for i, fixture in enumerate(fixtures):
         name = '%s_%03d' % (test_func.__name__, i)
