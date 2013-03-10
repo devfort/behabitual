devfort/behabitual

commit c360f6d9cf8e49b70c864321abec41ac315d1978

Add and test logged in homepage.

apps/habits/models.py
@@ -11,11 +11,10 @@ from django.utils.translation import ugettext_lazy as _
 record_habit_data = django.dispatch.Signal()
 
 RESOLUTION_NAMES = (
-    _('daily'),
-    _('on weekdays'),
-    _('on weekends'),
-    _('weekly'),
-    _('monthly'),
+    _('day'),
+    _('day on weekdays'),
+    _('day on weekends'),
+    _('week'),
 )
 
 RESOLUTIONS = (
@@ -23,7 +22,6 @@ RESOLUTIONS = (
     'weekday',
     'weekendday',
     'week',
-    'month',
 )
 
 
@@ -189,6 +187,10 @@ class Habit(models.Model):
 
         if streak:
             yield streak
+
+    def get_resolution_name(self):
+        resolution_index = RESOLUTIONS.index(self.resolution)
+        return RESOLUTION_NAMES[resolution_index]
 
     def __unicode__(self):
         return 'Habit(description=%s start=%s resolution=%s)' % (

apps/homepage/templatetags/humanize_repetition.py
@@ -1,0 +1,21 @@
+from django import template
+from django.utils.encoding import force_text
+from django.contrib.humanize.templatetags.humanize import apnumber
+
+register = template.Library()
+
+@register.simple_tag
+def humanize_repetition(habit):
+    resolution_name = force_text(habit.get_resolution_name())
+    if 1 == habit.target_value:
+      return "once a %s" % resolution_name
+    elif 2 == habit.target_value:
+      return "twice a %s" % resolution_name
+    elif 3 == habit.target_value:
+      return "%s times a <!-- lady, and I love you --> %s" % (
+        apnumber(habit.target_value), resolution_name
+    )
+    else:
+      return "%s times a %s" % (
+        apnumber(habit.target_value), resolution_name
+    )

apps/homepage/tests.py
@@ -1,0 +1,41 @@
+from django_webtest import TestCase
+from django.core.urlresolvers import reverse
+
+import datetime
+
+from apps.accounts.models import User
+from apps.habits.models import Habit
+
+
+class HomepageTest(TestCase):
+    def setUp(self):
+        self.user_password = '12345'
+        self.user = User.objects.create_user(
+            email='someone@example.com', password=self.user_password
+        )
+
+    # TODO: Logged out homepage
+    def test_logged_out_homepage(self):
+        response = self.client.get(reverse('homepage'))
+        self.assertEqual(200, response.status_code)
+        self.assertTemplateUsed("homepage/home.html")
+
+    def test_logged_in_homepage_is_dashboard(self):
+        self.client.login(
+            email=self.user.email, password=self.user_password
+        )
+        response = self.client.get(reverse('homepage'))
+        self.assertTemplateUsed("homepage/user_dashboard.html")
+
+    def test_dashboard_contains_habit(self):
+        self.client.login(
+            email=self.user.email, password=self.user_password
+        )
+        habit = Habit.objects.create(
+            description="Brush my teeth",
+            start=datetime.date.today(),
+            user=self.user,
+            resolution='day'
+        )
+        response = self.client.get(reverse('homepage'))
+        self.assertContains(response, habit.description, 1)

apps/homepage/views.py
@@ -1,0 +1,19 @@
+from django.views.generic import ListView, View, TemplateView
+from apps.habits.models import Habit
+
+class HomepageView(View):
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated():
+            homepage_view = UserDashboard.as_view()
+        else:
+            homepage_view = TemplateView.as_view(
+                template_name='homepage/home.html'
+            )
+        return homepage_view(request, *args, **kwargs)
+
+class UserDashboard(ListView):
+    model = Habit
+    template_name = 'homepage/user_dashboard.html'
+
+    def get_queryset(self):
+        return self.request.user.habits.all()

hobbit/settings.py
@@ -128,7 +128,8 @@ INSTALLED_APPS = (
     'south',
     'apps.accounts',
     'apps.encouragements',
-    'apps.habits'
+    'apps.habits',
+    'apps.homepage',
 )
 
 AUTH_USER_MODEL = 'accounts.User'

hobbit/urls.py
@@ -2,12 +2,14 @@ from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 
 import apps.accounts.views
+import apps.homepage.views
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name='homepage/home.html'), name='homepage'),
+    # url(r'^$', TemplateView.as_view(template_name='homepage/home.html'), name='homepage'),
+    url(r'^$', apps.homepage.views.HomepageView.as_view(), name='homepage'),
     url(r'^logout/$', apps.accounts.views.LogoutView.as_view(), name='logout'),
     url(r'^password-change/$', apps.accounts.views.password_change, name='password_change'),
     url(r'^password-change/done/$', 'django.contrib.auth.views.password_change_done', name='password_change_done'),

templates/homepage/home.html
@@ -2,5 +2,6 @@
 
 {% block title %}Home{% endblock %}
 {% block body %}
+{# TODO: Logged out homepage #}
   <h1>HOME BOOM</h1>
 {% endblock %}

templates/homepage/user_dashboard.html
@@ -1,0 +1,26 @@
+{% extends "base.html" %}
+{% load humanize_repetition %}
+{% block title %}Home{% endblock %}
+{% block body %}
+<section>
+  <h1>Progress</h1>
+  <img alt="Visual progress" src="#TODO">
+</section>
+
+<section>
+  <h1>Habits</h1>
+  
+  <ul>
+  {% for habit in habit_list %}
+    <li>
+      <h2><q>{{ habit.description }}</q>
+        {% humanize_repetition habit %}
+      </h2>
+      
+      {# TODO: Add habit descriptions to enter data/edit habit links? #}
+      <a href="#TODO">Enter data</a> <a href="#TODO">Edit habit</a>
+    </li>
+  {% endfor %}
+  </ul>
+</section>
+{% endblock %}

commit 1ceb8d058dc7a74d12aaff4cf4e28dbfeedcb10a

Reinstate 'month' resolution at the model level
Reverts part of c360f6d9, which broke a number of assumptions made in the
model layer about the existence of the month resolution.

Provides RESOLUTIONS_NO_MONTH for parts of the UI which need a set of
resolution choices without months.

apps/habits/models.py
@@ -10,21 +10,20 @@ from django.contrib.humanize.templatetags.humanize import ordinal
 
 from .signals import habit_archived, habit_created, habit_data_recorded
 
-RESOLUTION_NAMES = (
-    _('day'),
-    _('day on weekdays'),
-    _('day on weekends'),
-    _('week'),
-)
-
 RESOLUTIONS = (
-    'day',
-    'weekday',
-    'weekendday',
-    'week',
+    ('day',        _('day')),
+    ('weekday',    _('day on weekdays')),
+    ('weekendday', _('day on weekends')),
+    ('week',       _('week')),
+    ('month',      _('month')),
 )
 
-RESOLUTION_CHOICES = zip(RESOLUTIONS, RESOLUTION_NAMES)
+RESOLUTIONS_NO_MONTH = (
+    ('day',        _('day')),
+    ('weekday',    _('day on weekdays')),
+    ('weekendday', _('day on weekends')),
+    ('week',       _('week')),
+)
 
 def _validate_non_negative(val):
     if val < 0:
@@ -207,7 +206,7 @@ class Habit(models.Model):
     user = models.ForeignKey('accounts.User', related_name='habits')
     resolution = models.CharField(
         max_length=10,
-        choices=RESOLUTION_CHOICES,
+        choices=RESOLUTIONS,
         default='day',
     )
     start = models.DateField()
@@ -347,8 +346,9 @@ class Habit(models.Model):
             yield streak
 
     def get_resolution_name(self):
-        resolution_index = RESOLUTIONS.index(self.resolution)
-        return RESOLUTION_NAMES[resolution_index]
+        for ident, name in RESOLUTIONS:
+            if self.resolution == ident:
+                return name
 
     def set_reminder_schedule(self, weekdays, hour):
         """
@@ -407,7 +407,7 @@ class Bucket(models.Model):
     )
     resolution = models.CharField(
         max_length=10,
-        choices=zip(RESOLUTIONS, RESOLUTION_NAMES),
+        choices=RESOLUTIONS,
         default='day',
     )
 

apps/onboarding/forms.py
@@ -1,7 +1,11 @@
 from django import forms
 from django.contrib.auth import get_user_model
+<<<<<<< HEAD
 from django.utils.safestring import mark_safe
 from apps.habits.models import RESOLUTION_CHOICES
+=======
+from apps.habits.models import RESOLUTIONS_NO_MONTH
+>>>>>>> Reinstate 'month' resolution at the model level
 
 User = get_user_model()
 
@@ -33,7 +37,7 @@ class HabitForm(forms.Form):
         widget=forms.TextInput(attrs={'placeholder': '2'}),
     )
     resolution = forms.ChoiceField(
-        choices=RESOLUTION_CHOICES,
+        choices=RESOLUTIONS_NO_MONTH,
     )
 
 