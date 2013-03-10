7b977d08aa0fb75f105b1c46ffb6fc2e60195cbc
George Brocklehurst <george.brocklehurst@gmail.com>
2013-03-10 19:35:07 +0000

    Beginnings of a settings page.

--- hobbit/urls.py
+++ hobbit/urls.py
@@ -19,6 +19,8 @@ urlpatterns = patterns('',
     url(r'^logout/$', apps.accounts.views.LogoutView.as_view(), name='logout'),
     url(r'^password-change/$', apps.accounts.views.password_change, name='password_change'),
     url(r'^password-change/done/$', 'django.contrib.auth.views.password_change_done', name='password_change_done'),
+    secured_url(r'^accounts/settings', TemplateView.as_view(template_name='accounts/settings.html'), name='account_settings'),
+
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^reset/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
