83496d937b57ae6135d25e28aaa096220d038cbb
Steve Marshall <steve@nascentguruism.com>
2013-03-10 23:30:31 +0000

    Fix homepage onboarding form.

--- apps/homepage/views.py
+++ apps/homepage/views.py
@@ -1,14 +1,13 @@
 from django.views.generic import ListView, View, TemplateView
 from apps.habits.models import Habit
+from apps.onboarding.forms import HabitForm
 
 class HomepageView(View):
     def get(self, request, *args, **kwargs):
         if request.user.is_authenticated():
             homepage_view = UserDashboard.as_view()
         else:
-            homepage_view = TemplateView.as_view(
-                template_name='homepage/home.html'
-            )
+            homepage_view = LoggedOutHomepageView.as_view()
         return homepage_view(request, *args, **kwargs)
 
 class UserDashboard(ListView):
@@ -20,3 +19,12 @@ class UserDashboard(ListView):
 
     def is_dashboard(self):
         return True
+
+class LoggedOutHomepageView(TemplateView):
+    template_name = 'homepage/home.html'
+    
+    def get_context_data(self, **kwargs):
+        ctx = super(LoggedOutHomepageView, self).get_context_data(**kwargs)
+        ctx['form'] = HabitForm(prefix="habit")
+        return ctx
+    
