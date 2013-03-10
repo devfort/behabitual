26e5ad484f1cedf3e03d6a3436ce3b35b0158bf1
mazz <marianne.mosley@gmail.com>
2013-03-10 22:22:36 +0000

    Statsd all the things.

--- apps/monitoring.py
+++ apps/monitoring.py
@@ -1,12 +1,18 @@
 from django.contrib.auth.signals import user_logged_out, user_logged_in
 from django.dispatch import receiver
+from django.db.models.signals import post_save
 
+from apps.accounts.models import User
 from apps.accounts.signals import user_changed_password
 from apps.habits.signals import (habit_archived,
                                  habit_created,
                                  habit_data_recorded)
 from lib.metrics import statsd
 
+@receiver(post_save, sender=User)
+def record_user_creation(sender, **kwargs):
+    if kwargs['created']:
+        statsd.incr('user.created')
 
 @receiver(user_logged_out)
 def record_user_logged_out(sender, **kwargs):
