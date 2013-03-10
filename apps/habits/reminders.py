f8d4d1be30628f32ebe38b025f4dc091cf457fe7
Nick Stenning <nick@whiteink.com>
2013-03-10 23:03:44 +0100

    Add send_data_collection_email reminder
    Add a send_data_collection_email function to apps.habits.reminders, which
    takes a habit and, if appropriate, sends a data collection email for that
    habit.

--- apps/habits/reminders.py
+++ apps/habits/reminders.py
@@ -1,10 +1,64 @@
+import datetime
+
+from django.utils.translation import ugettext_lazy as _
+
 from util.render_to_email import render_to_email
 
 def send_reminder_email(habit):
-    render_to_email(
+    return render_to_email(
         text_template='habits/emails/reminder.txt',
         html_template='habits/emails/reminder.html',
         to=(habit.user,),
         subject=habit.description,
         context=dict(description=habit.description),
     )
+
+def send_data_collection_email(habit, today=None):
+    if today is None:
+        today = datetime.date.today()
+
+    if not habit.send_data_collection_emails:
+        return
+
+    if today == habit.start:
+        # Don't ask for data from before the habit was created
+        return
+
+    if habit.resolution == 'weekday':
+        # Don't ask for data on Sundays and Mondays
+        if today.weekday() in [6, 0]:
+            return
+
+    if habit.resolution == 'weekendday':
+        # Don't ask for data from Tuesday to Saturday
+        if today.weekday() in range(1, 6):
+            return
+
+    if habit.resolution == 'week':
+        # Don't ask for data unless today is a Monday
+        if today.weekday() != 0:
+            return
+
+    if habit.resolution == 'month':
+        # Don't ask for data unless today is the 1st of a new month
+        if today.day != 1:
+            return
+
+    time_period_name = {
+        'day':        _('yesterday'),
+        'weekday':    _('yesterday'),
+        'weekendday': _('yesterday'),
+        'week':       _('last week'),
+        'month':      _('last month'),
+    }[habit.resolution]
+
+    context = {'time_period_name': time_period_name}
+
+    return render_to_email(
+        text_template='habits/emails/data_collection.txt',
+        html_template='habits/emails/data_collection.html',
+        to=(habit.user,),
+        subject='Let us know how you did',
+        context=context,
+    )
+
--- apps/habits/tests/test_reminders.py
+++ apps/habits/tests/test_reminders.py
@@ -5,17 +5,69 @@ from django.test import TestCase
 
 from apps.accounts.models import User
 from apps.habits.models import Habit
-from apps.habits.reminders import send_reminder_email
+from apps.habits.reminders import send_reminder_email, send_data_collection_email
+from lib import test_helpers as helpers
+
+DATA_COLLECTION_FIXTURES = (
+    # habit resolution, send date, should send?
+    ('day', '2013-03-04', False), # shouldn't send if only just created
+    ('day', '2013-03-05', True),
+    ('day', '2013-03-06', True),
+    ('day', '2013-03-07', True),
+    ('weekday', '2013-03-08', True),
+    ('weekday', '2013-03-09', True),
+    ('weekday', '2013-03-10', False),
+    ('weekday', '2013-03-11', False),
+    ('weekday', '2013-03-12', True),
+    ('weekendday', '2013-03-09', False),
+    ('weekendday', '2013-03-10', True),
+    ('weekendday', '2013-03-11', True),
+    ('weekendday', '2013-03-12', False),
+    ('week', '2013-03-10', False),
+    ('week', '2013-03-11', True),
+    ('week', '2013-03-12', False),
+    ('month', '2013-03-13', False),
+    ('month', '2013-03-31', False),
+    ('month', '2013-04-01', True),
+    ('month', '2013-04-02', False),
+)
 
 class TestReminders(TestCase):
 
-    def test_send_reminder_email(self):
-        u = User.objects.create(email='foo@bar.com', password='SecretZ!')
-        h = Habit.objects.create(user=u,
-                                 description='Frobble your wingdangle',
-                                 start=datetime.date.today())
+    def setUp(self):
+        self.u = User.objects.create(email='foo@bar.com', password='SecretZ!')
+        self.h = Habit.objects.create(user=self.u,
+                                      description='Frobble your wingdangle',
+                                      start=datetime.date(2013, 3, 4))
 
-        send_reminder_email(h)
+    def test_send_reminder_email(self):
+        send_reminder_email(self.h)
 
         self.assertEqual(len(mail.outbox), 1)
         self.assertTrue('Frobble your wingdangle' in mail.outbox[0].subject)
+
+    def test_disable_send_data_collection_email(self):
+        self.h.send_data_collection_emails = False
+
+        result = send_data_collection_email(self.h)
+
+        self.assertIsNone(result)
+        self.assertEqual(len(mail.outbox), 0)
+
+
+def test_send_data_collection_email(self, fixture):
+    resolution, today, should_send = fixture
+    today_date = helpers.parse_isodate(today)
+
+    self.h.resolution = resolution
+    result = send_data_collection_email(self.h, today=today_date)
+
+    if should_send:
+        self.assertIsNotNone(result)
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertTrue('Let us know' in mail.outbox[0].subject)
+    else:
+        self.assertIsNone(result)
+        self.assertEqual(len(mail.outbox), 0)
+
+helpers.attach_fixture_tests(TestReminders, test_send_data_collection_email, DATA_COLLECTION_FIXTURES)
