f71a06f50530c04218a9e2a33a26eaced5eba84e
George Brocklehurst <george.brocklehurst@gmail.com>
2013-03-17 20:38:50 +0100

    PREPEND_WWW!

--- hobbit/settings.py
+++ hobbit/settings.py
@@ -209,5 +209,6 @@ if 'true' == env.get('FULLY_SECURE'):
     SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https") # Heroku sends this
 
 ALLOWED_HOSTS = env.get('ALLOWED_HOSTS', 'localhost').split(';')
+PREPEND_WWW = True
 
 GOOGLE_ANALYTICS_ID = env.get('GOOGLE_ANALYTICS_ID', 'UA-39256165-2')
