902f276aae02623fe76df8a8f047d2aaa8d087be
Anna Shipman <anna@annashipman.co.uk>
2013-06-11 20:50:55 +0100

    Tests now fail correctly
    As in, this is the result I expect

--- apps/habits/tests/test_models.py
+++ apps/habits/tests/test_models.py
@@ -189,13 +189,13 @@ RECENT_TIME_PERIODS_FIXTURES = (
     # Today is 5th March.
     # Therefore, we expect data from the five days between (and including) 1st
     # and 5th March.
-    RTPT('2013-03-01', 'day', (), '2013-03-05', [4,3,2,1,0]),
+    RTPT('2013-03-01', 'day', (), '2013-03-05', [3,2,1,0]),
     # Habit started 1st March, daily, data has been entered for March 3rd.
     # Today is 5th March.
     # Therefore we expect data for the 4th, and the 5th of March (i.e. the 3rd
     # and 4th time period).
     # We have written off the 0th and 1st time period (1st, 2nd March).
-    RTPT('2013-03-01', 'day', (('2013-03-03',1),), '2013-03-05', [4,3]),
+    RTPT('2013-03-01', 'day', (('2013-03-03',1),), '2013-03-05', [3]),
     # Habit started 1st March, daily, data has been entered for 5th March.
     # Today is 5th March.
     # Therefore, we consider ourselves to be up-to-date with data.
@@ -207,18 +207,18 @@ RECENT_TIME_PERIODS_FIXTURES = (
     # The TimePeriod returned is this:
     # [TimePeriod(resolution='week', index=0, date=datetime.date(2013, 4, 29))]
     # i.e. it expects data for the w/c 29th April. I say that's wrong.
-    RTPT('2013-05-01', 'week', (), '2013-05-02', [0]),
+    RTPT('2013-05-01', 'week', (), '2013-05-02', []),
     # Habit started 1st May, weekly, data has been entered for 2nd week
     # Today is 29th May.
     # Therefore we expect data for the 3rd and 4th week
     # I think it should be only the 3rd week
-    RTPT('2013-05-01', 'week', (('2013-05-13',1),), '2013-05-29', [4,3]),
+    RTPT('2013-05-01', 'week', (('2013-05-13',1),), '2013-05-29', [3]),
     # Habit started 1st May, weekly.
     # Data has been entered for 1st week, 2nd week and 4th week
     # Today is 18th June.
     # Therefore we expect data for the 5th, 6th and 7th week
     # I think it should be only the 5th and 6th
-    RTPT('2013-05-01', 'week', (('2013-05-06',1),('2013-05-13',1),('2013-05-27',1)), '2013-06-18', [7,6,5]),
+    RTPT('2013-05-01', 'week', (('2013-05-06',1),('2013-05-13',1),('2013-05-27',1)), '2013-06-18', [6,5]),
 )
 
 TPFNF = namedtuple('TimePeriodFriendlyNameFixture', 'start resolution relative expected')
