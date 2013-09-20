6b39efe078f15068643e6a23cc7b0b64710c0063
L. Sandvik <linda.sandvik@googlemail.com>
2013-09-20 10:08:19 +0100

    fix typo Bruce Waynme -> Bruce Wayne

--- apps/encouragements/strings.py
+++ apps/encouragements/strings.py
@@ -9,7 +9,7 @@ ENCOURAGEMENT_0 = (
   "Best in show... where it's your show.",
   "<meta>Gif of norm dancing</meta>",
   "Like a boss.",
-  "Bruce Waymne looks up to you.",
+  "Bruce Wayne looks up to you.",
   "Up and atom!",
   "In 1972 a crack commando squad was sentenced to prison by a military court for a crime they didn't commit. They promptly escaped from a maximum security stockade to the Los Angeles underground. Today, still wanted by the government, they survive as soldiers of fortune. If you have a problem, if no-one else can help, and if you can find them, maybe YOU ARE THE A-TEAM.",
   "Puppies!",
